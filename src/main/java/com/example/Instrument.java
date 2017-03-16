fcd7a7f1230999c46508df5c35872509268609eb
Christian Gebauer <mail@christiangebauer.de>
2017-03-16 02:42:10 +0100

    docker ready

--- src/main/java/com/example/Instrument.java
+++ src/main/java/com/example/Instrument.java
@@ -5,17 +5,15 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Data
 @Entity
-@Table(name = "[20170223 xffm2_instruments_structs]")
 public class Instrument {
 
   @Id
-  @Column(name = "isoisincod")
+  @Column(name = "isin")
   private String isoIsinCod;
 
-  @Column(name = "instshtnam")
+  @Column(name = "name")
   private String instShtNam;
 }
